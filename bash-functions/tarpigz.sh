6f4e82b8e00125771f1b79404c2eb8b44ff789cb
Romolo de Almeida Alves <peo_ralves@uolinc.com>
2020-11-23 13:31:38 -0300

    Compress a file folder to an tar gzip file through many processors using the 'pigz' utility coupled by pipeline.

--- bash-functions/tarpigz.sh
+++ bash-functions/tarpigz.sh
@@ -1,3 +1,5 @@
+# Compress a file folder to an tar gzip file through many processors
+# using the 'pigz' utility coupled by pipeline.
 function tarpigz() {
   if [[ "$1" == "" || "$2" == "" ]]; then echo "Usage: $FUNCNAME <<DestinationCompressedTarGzFile>> <<SourceDirOrFilename>>" ; return 1 ; fi
   if [[ ! -e "$2" ]]; then echo "Error: source file/folder '$2' not found."; return 1 ; fi
