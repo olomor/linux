e02c8143809dd63e47cd89712e4f8339a9e003e9
Romolo de Almeida Alves <peo_ralves@uolinc.com>
2020-11-23 13:20:36 -0300

    Overlap of iostat. Filter command output on bypass first sample of data, that was the accumulated result from system boot time and its not relevant.

--- bash-functions/iostat2.sh
+++ bash-functions/iostat2.sh
@@ -1,3 +1,9 @@
+#----------------------------------------------------------------------
+# iostat command overlap
+#
+#   Filter command output on bypass first sample of data, that was the 
+#   accumulated result from system boot time and its not relevant.
+#
 function iostat2() {
   FILTER=$(iostat -x 1 1 |wc -l)
   iostat ${@} |tail -n +$((FILTER+1))
